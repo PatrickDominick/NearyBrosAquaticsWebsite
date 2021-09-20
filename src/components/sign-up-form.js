8d3df2999895e3579198c40eab7a87b9323923c8
PatrickDominick <pdneary@gmail.com>
2021-09-20 16:10:59 -0500

    added a spacer100 to helpers

--- src/components/sign-up-form.js
+++ src/components/sign-up-form.js
@@ -102,7 +102,7 @@ export default class SignupForm extends Component {
                     </div>
                 </div>
               </div>
-                <div className="login-form-wrapper">
+                <div className="signup-form-wrapper">
                   <input type="text" 
                   placeholder="Username"
                   name="username"
@@ -138,6 +138,7 @@ export default class SignupForm extends Component {
                 <p>{this.state.error}</p>
                 <p>Have an account?</p>
                 <Link to="/login-form"><button className="btn" >Log In/Sign Up</button></Link>
+                <div className="spacer100"/>
                 <Footer />
               </div>
             </form>
